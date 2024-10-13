754437a87006a5d827dca9a01652b93fc72ced56
Suckton_Hale <gogosha115@gmail.com>
2024-10-13 10:01:00 +0300

    Commit

--- Dz3.py
+++ Dz3.py
@@ -7,3 +7,14 @@ class Cat():
         print(f"{self.name} casted basic attac with dmg {self.damage}")
     def combo1(self):
         print(f"{self.name} made combo with dmg {self.damage} ")
+my_cat = Cat('Chert',10,2500)
+my_cat.attack()
+my_cat = Cat("Chert",10,5070)
+my_cat.combo1()
+class Dog():
+    def __init__(self,name):
+        self.name = name
+    def Sleep(self):
+        print(f'{self.name} is sleeping :)')
+my_dog = Dog('Willie')
+my_dog.Sleep()
